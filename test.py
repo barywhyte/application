1c93c60e97edef5d565fd172f74bf0121105b2e6
seun ore <seunsamore@gmail.com>
2020-07-28 11:48:17 +0100

    remove empty space

--- test.py
+++ test.py
@@ -18,7 +18,7 @@ from app import hello
 class TestHelloApp(unittest.TestCase):
 
   def test_hello(self):
-    self.assertEqual(hello(), "Hello seun in the  World!\n")
+    self.assertEqual(hello(), "Hello seun in the World!\n")
 
 if __name__ == '__main__':
   unittest.main()
